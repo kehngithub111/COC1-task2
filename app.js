2868c4abea7ffcddb55714482eab2a8bf7823810
kehn <kehngithub2301@gmail.com>
2024-10-26 16:30:06 +0800

    Changes 10-26-24

--- app.js
+++ app.js
@@ -3,4 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const date = new Date().getFullYear();
 
     footerContent.textContent = `All Rights Reserved ${date}`;
+
+
+    const navButton = document.getElementById('navButton');
+    const navContainer = document.getElementById('navContainer');
+    let navButtonClick = 0;
+
+    navButton.addEventListener('click', () => {
+        navButtonClick++;
+        console.log(navButtonClick);
+
+        if (navButtonClick % 2 === 1) {
+            navContainer.classList.add('navActive');
+            document.body.style.overflow = 'hidden';
+        }else {
+            navContainer.classList.remove('navActive');
+            document.body.style.overflow = 'auto';
+        }
+    });
 });
